88eab9a95e9208117b52f1b0cd207d0aeacbd8b0
Ableez <djayableez@gmail.com>
2024-05-11 00:15:21 +0100

    Error component fix

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import PageDataFetchError from "@/components/PageDataFetchError";
-import { Button } from "@/components/ui/button";
 import { useEffect } from "react";
 
 export default function Error({
@@ -15,35 +14,5 @@ export default function Error({
     console.error(error);
   }, [error]);
 
-<<<<<<< HEAD
   return <PageDataFetchError error={error.message} />;
-=======
-  return (
-    // <section className="bg-white dark:bg-black h-screen">
-    //   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-    //     <div className="mx-auto max-w-screen-md text-center">
-    //       <h1 className="mb-4 text-4xl md:text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
-    //         Whoops!
-    //       </h1>
-    //       <p className="mb-4 text-lg tracking-tight font-bold text-neutral-900 md:text-4xl dark:text-white">
-    //         We are sorry, but something went wrong.
-    //       </p>
-    //       <p className="mb-4 text-sm font-light text-neutral-500 dark:text-neutral-400">
-    //         Please try again.
-    //       </p>
-    //     </div>
-    //     <div className="w-full grid place-items-center justify-center">
-    //       <Button
-    //         className="mx-auto px-16 hover:ring-pink-200 ring-4 ring-transparent duration-300"
-    //         onClick={() => reset()}
-    //       >
-    //         Retry
-    //       </Button>
-    //     </div>
-    //   </div>
-    // </section>
-
-    <PageDataFetchError error={error.message} />
-  );
->>>>>>> 9bd3d802196afbaf2bb576fec5fa750d715bcb2b
 }
