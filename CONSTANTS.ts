10b0380eccc33ca4af72885a60025228c0d1285f
Ableez <djayableez@gmail.com>
2024-07-07 16:31:55 +0100

    Emails Changes

--- CONSTANTS.ts
+++ CONSTANTS.ts
@@ -1,5 +1,1 @@
-export const adminEmails = [
-  "LYKze6RrldQ2bPNIBBHx5N19VPF3", //maxx
-  "kLrH8mCfhdNnEd3wKF36PYbw1A32", //djayableez
-  "cYN0b7LG9JdffQ3lRn2NgamEJkt1", //admin
-];
+export const adminEmails = JSON.parse(process.env.ADMIN_EMAILS!);
