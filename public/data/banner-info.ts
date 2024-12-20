b276f3d3b1f440ea9cf48adb6652ad2a9fa9fb8a
Ahmed Abdullahi <djayableez@gmail.com>
2024-12-20 02:00:43 +0100

    updated site banner information

--- public/data/banner-info.ts
+++ public/data/banner-info.ts
@@ -1,10 +1,3 @@
 import { type BannerInfo } from "@/components/banner/banner";
 
-export const bannerInfoList: BannerInfo[] = [
-  {
-    title: "WhatsApp Number Change",
-    body: "We have changed our WhatsApp number to 08165837925. Please update your contacts.",
-    interval: 24 * 60 * 60 * 1000, // 24 hours
-    autoClose: false,
-  },
-];
+export const bannerInfoList: BannerInfo[] = [];
